7b5d553a4422268cd3127a1337c795f1d111563e
CarlosLabrado <carlos@petrolog.us>
2017-12-01 18:57:35 -0700

    Fix: Some dependencies can't be build into the new android releases.

--- app/src/main/java/com/ocr/termocel/FirstApplication.java
+++ app/src/main/java/com/ocr/termocel/FirstApplication.java
@@ -4,26 +4,25 @@ import android.app.Application;
 import android.content.Context;
 
 import com.activeandroid.ActiveAndroid;
-import com.path.android.jobqueue.JobManager;
-import com.path.android.jobqueue.config.Configuration;
-import com.path.android.jobqueue.log.CustomLogger;
 
 /**
  * This class initializes all the goodies on the app
  */
 public class FirstApplication extends Application {
 
+    public static Context context;
     private static FirstApplication instance;
-    private JobManager jobManager;
+
+    public FirstApplication() {
+        instance = this;
+    }
 
     public static Context getContext() {
         return context;
     }
 
-    public static Context context;
-
-    public FirstApplication() {
-        instance = this;
+    public static FirstApplication getInstance() {
+        return instance;
     }
 
     @Override
@@ -34,52 +33,9 @@ public class FirstApplication extends Application {
 
         context = getApplicationContext();
 
-        configureJobManager();
 
 //        Firebase.setAndroidContext(this);
 
-    }
-
-    private void configureJobManager() {
-        Configuration configuration = new Configuration.Builder(this)
-                .customLogger(new CustomLogger() {
-                    private static final String TAG = "JOBS";
-
-                    @Override
-                    public boolean isDebugEnabled() {
-                        return true;
-                    }
-
-                    @Override
-                    public void d(String text, Object... args) {
-//                        Log.d(TAG, String.format(text, args));
-                    }
-
-
-                    @Override
-                    public void e(Throwable t, String text, Object... args) {
-//                        Log.e(TAG, String.format(text, args), t);
-                    }
-
-                    @Override
-                    public void e(String text, Object... args) {
-//                        Log.e(TAG, String.format(text, args));
-                    }
-                })
-                .minConsumerCount(1)//always keep at least one consumer alive
-                .maxConsumerCount(3)//up to 3 consumers at a time
-                .loadFactor(3)//3 jobs per consumer
-                .consumerKeepAlive(120)//wait 2 minute
-                .build();
-        jobManager = new JobManager(this, configuration);
-    }
-
-    public JobManager getJobManager() {
-        return jobManager;
-    }
-
-    public static FirstApplication getInstance() {
-        return instance;
     }
 
     @Override
